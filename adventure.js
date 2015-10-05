efb213e2f1b6ce9be14d57dd9af5e55bdfc0786b
d228m3286w <d228m3286w@gmail.com>
2015-10-05 14:35:05 -0600

    not working grid

--- adventure.js
+++ adventure.js
@@ -1,36 +1,55 @@
 
-function Adventure(yCoord, xCoord, endX, endY, bound){
-	this.yCoord = yCoord;
-	this.xCoord = xCoord;
-	this.endX = endX;
-	this.endY = endY;
+/////////////////////////////////Make and move Abe//////////////////////////////////
+
+function Adventure(xCoord, yCoord, endX, endY, bound) {
+	this.coord = new Thing(xCoord, yCoord);
+	this.end = new Thing(endX, endY);
+	// this.xCoord = xCoord;
+	// this.yCoord = yCoord;
 	this.bound = bound;
 };
 
 Adventure.prototype.killAbe = function () {
-	document.getElementById(this.endY.toString() + 
-		this.endX.toString()).innerHTML = "<img src='dead.jpg' class='img'></img>";
+	document.getElementById(this.end.y.toString() + 
+		this.end.x.toString()).innerHTML = "<img src='dead.jpg' class='img'></img>";
 };
 
 Adventure.prototype.target = function(){
-	document.getElementById(this.endY.toString() + 
-		this.endX.toString()).innerHTML = "<img src='retire.png' class='img'></img>";
+	document.getElementById(this.end.y.toString() + 
+		this.end.x.toString()).innerHTML = "<img src='retire.png' class='img'></img>";
 };
 
 Adventure.prototype.movAbe = function(){
-	document.getElementById(this.yCoord.toString() + 
-		this.xCoord.toString()).innerHTML = "<img src='small_abe.png' class='img'></img>";
+	document.getElementById(this.coord.x.toString() + 
+		this.coord.y.toString()).innerHTML = "<img src='small_abe.png' class='img'></img>";
 };
 
 Adventure.prototype.hideAbe = function (xChange, yChange) {
-	document.getElementById((this.yCoord + 
-		yChange).toString() + (this.xCoord + xChange).toString()).innerHTML = "";
+	document.getElementById(this.coord.x + xChange).toString() + (this.coord.y + yChange).toString()).innerHTML = "";
 };
 
-var adventure = new Adventure(0,0,8,8,8);
+// Adventure.prototype.movBad = function() {
+// 	document.getElementById(this.yCoord.toString() + 
+// 		this.xCoord.toString()).innerHTML = "<img src='small_abe.png' class='img'></img>";
+// };
+///////////////////////////////////////Move Bad Guy//////////////////////////////////////////
 
+// function BadAss(yCoord, xCoord, endX, endY, bound){
+// 	this.yCoord = yCoord;
+// 	this.xCoord = xCoord;
+	// this.endX = endX;
+	// this.endY = endY;
+// 	this.bound = bound;
+// };
 
+// BadAss.prototype.movBad = function() {
+// 	document.getElementById(this.yCoord.toString() + 
+// 		this.xCoord.toString()).innerHTML = "<img src='small_abe.png' class='img'></img>";
+// };
 
+// }
+
+///////////////////////////////////Gird set up///////////////////////////////////////////////
 
 
 function Grid(rows,cols) {
@@ -51,6 +70,16 @@ function Grid(rows,cols) {
 
 
 
+//////////////////////////thing///////////////////////////////////
+
+function Thing(x, y) {
+	this.x = x;
+	this.y = y;
+
+
+
+	
+}
 
 
 
--- app.js
+++ app.js
@@ -3,50 +3,49 @@ var holdon = new Audio('holdon.wav');
 var coon = new Audio('racoon.wav');
 var dead = new Audio('dead.wav');
 var grid;
-
-
-
 var adventure;
+// var thing = new Thing(9,9);
+
+
 
     function setUp() {
         console.log("hello");
         grid = new Grid(9, 9);
         grid.makeTable();
-        adventure = new Adventure(0,0,8,8,8);
+        adventure = new Adventure(0,0,grid.rows -1,grid.cols -1, grid.rows );
         adventure.movAbe();
         adventure.target();
 };
-// function name() {
-//     var xcoord = prompt("enter how many rows you want");
-//     var ycoord = prompt("enter how many columes you want")
-//     grid = new Grid(xcoord, ycoord);
-    
-// };
+
 
 
 function mov(xDel, yDel, x, y, bound) {
 	
-	if(adventure.xCoord === (adventure.endX + xDel) && adventure.yCoord === (adventure.endY + yDel)) {
-        adventure.xCoord += x;
-        adventure.yCoord += y;
+	if(adventure.coord.x === (adventure.end.x + xDel) && adventure.coord.y === (adventure.end.y + yDel)) {
+        adventure.coord.x += x;
+        adventure.coord.y += y;
+
         adventure.hideAbe(xDel, yDel);	
         adventure.killAbe();
         dead.play();
         alert("Whuuthaa!!??");
 	}
-	else if (adventure.yCoord === bound && yDel != 0) {
+	else if (adventure.coord.y === bound && yDel != 0) {
         holdon.play();
         alert("D'oh!!!");
     }
-    else if (adventure.xCoord === bound && xDel != 0) {
+    else if (adventure.coord.x === bound && xDel != 0) {
         coon.play();
         alert("D'oh!!!");
     }
     else {
-        adventure.xCoord += x;
-        adventure.yCoord += y;
+        adventure.coord.x += x;
+        adventure.coord.y += y;
+        console.log("mov: " + adventure.coord.x + adventure.coord.y);
+        adventure.movAbe();
         adventure.hideAbe(xDel, yDel);
-		adventure.movAbe();
+
+		
 	};
 }
 
@@ -54,22 +53,22 @@ function mov(xDel, yDel, x, y, bound) {
 
 
 function movDown() {
-	mov(0, -1, 0, 1, 8);
+	mov( -1, 0, 1, 0, 8);
 }; 
 function movUp() {
-	mov(0, 1, 0, -1, 0);
-}; 
-function movLeft() {
 	mov(1, 0, -1, 0, 0);
 }; 
+function movLeft() {
+	mov(0, 1, 0, -1, 0);
+}; 
 function movRight() {
-	mov(-1, 0, 1, 0, 8);
+	mov(0, -1, 0, 1, 8);
 };
 
 function reset() {
     adventure.hideAbe(0,0);
-    adventure.xCoord = 0;
-    adventure.yCoord = 0;
+    adventure.coord.x = 0;
+    adventure.coord.y = 0;
     adventure.movAbe();
     adventure.target();
 }
@@ -93,5 +92,14 @@ document.onkeydown = function(e) {
             break;
    }
 };
+
+
+
+// function name() {
+//     var xcoord = prompt("enter how many rows you want");
+//     var ycoord = prompt("enter how many columes you want")
+//     grid = new Grid(xcoord, ycoord);
+    
+// };
 
 
